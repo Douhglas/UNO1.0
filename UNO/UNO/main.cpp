3f4a0da975093f0e39a1c74942fb36fba92c8b3d
Ahhabb <douglas.ramirez.morales@est.una.ac.cr>
2023-09-26 14:55:35 -0600

    en el main se crearon varias funciones para organizar la entrada de eventos de cada modo de juego

--- UNO/UNO/main.cpp
+++ UNO/UNO/main.cpp
@@ -12,6 +12,33 @@ void singlePlayerWindow(sf::RenderWindow& window, Card& card, sf::Mouse& mouse, 
 void multiPlayerWindow(sf::RenderWindow& window, Card& card, sf::Mouse& mouse, bool& isRunningGame, bool& isMultiPlayer, bool& isLeftTurn,
 	bool& startGame, bool& isEndGame, int& atLeastOneCard, int& CardsOfTheSameNumberPerTurn, int& pluscCardLimitperTurn, bool& isUnobuttonPressed);
 
+void closeWindowSinglePlayer(sf::Event& even, sf::RenderWindow& window, bool& isSinglePlayer);
+
+void closeWindowMultiPlayer(sf::Event& even, sf::RenderWindow& window, bool& isMultiPlayer);
+
+void startGamee(sf::Event& even, sf::RenderWindow& window, Card& card, sf::Mouse& mouse, int& color, int& numCard, bool& startGame);
+
+void getCard(sf::Event& even, sf::RenderWindow& window, Card& card, sf::Mouse& mouse, int& color, int& numCard, bool& isEndGame, bool& isLeftTurn);
+
+void endTheTurnMultiPlayer(sf::Event& even, Card& card, bool& isLeftTurn, int& atLeastOneCard, int& CardsOfTheSameNumberPerTurn,
+	int& pluscCardLimitperTurn);
+
+void endTheTurnSinglePlayer(sf::Event& even, Card& card, bool& isLeftTurn, int& atLeastOneCard, int& CardsOfTheSameNumberPerTurn,
+	int& pluscCardLimitperTurn);
+
+void PressUnoButtonMultiPlayer(sf::Event& even, Card& card, bool& isLeftTurn, int& atLeastOneCard, bool& isUnobuttonPressed);
+
+void PressUnoButtonSinglePlayer(sf::Event& even, Card& card, bool& isLeftTurn, int& atLeastOneCard, bool& isUnobuttonPressed);
+
+void throwCardMultiPlayer(sf::Event& even, sf::RenderWindow& window, sf::Mouse& mouse, Card& card, bool& isLeftTurn, int& atLeastOneCard,
+	int& pluscCardLimitperTurn, bool& isEndGame, int& CardsOfTheSameNumberPerTurn);
+
+void throwCardSinglePlayer(sf::Event& even, sf::RenderWindow& window, sf::Mouse& mouse, Card& card, bool& isLeftTurn, int& atLeastOneCard,
+	int& pluscCardLimitperTurn, bool& isEndGame, int& CardsOfTheSameNumberPerTurn);
+
+void playComputerTurn(sf::Event& even, Card& card, bool& isLeftTurn, int& atLeastOneCard, int& CardsOfTheSameNumberPerTurn,
+	int& pluscCardLimitperTurn, bool& isUnobuttonPressed, int& color, int& numCard);
+
 
 int main() {
 	srand(time(NULL));
@@ -117,135 +144,24 @@ void singlePlayerWindow(sf::RenderWindow& window, Card& card, sf::Mouse& mouse, 
 
 			while (window.pollEvent(even)) {
 
-				if (even.type == even.Closed) {
+				closeWindowSinglePlayer(even,window,isSinglePlayer);
 
-					window.close();
-					isSinglePlayer = false;
+				startGamee(even,window,card,mouse,color,numCard,startGame);
 
-				}
-				if (even.type == even.MouseButtonPressed && card.isButtonPressed(2, window, mouse, even)) {
-					card.initPileCard(color, numCard);
-					card.init8CardsPerDeck();
-					startGame = true;
-					card.RemoveButton(2);
-				}
+				getCard(even, window, card, mouse, color, numCard, isEndGame, isLeftTurn);
 
-				if (!isEndGame) {
-					if (even.type == even.MouseButtonPressed) {
-						if (card.mainDeckIsPressed(window, mouse, even)) {
-							card.getTypeCard(color, numCard, isLeftTurn);
-						}
-					}
-				}
-				if (even.type == even.KeyPressed) {
-					if (even.key.code == sf::Keyboard::Enter) {
-						if ((isLeftTurn == true && atLeastOneCard != 0) && card.isNotCardsToEat() == true ||
-							((isLeftTurn == true && atLeastOneCard != 0) && (card.getPile().getNumber() == 12 || card.getPile().getNumber() == 16))) {
+				endTheTurnSinglePlayer(even, card, isLeftTurn, atLeastOneCard, CardsOfTheSameNumberPerTurn, pluscCardLimitperTurn);
 
-							isLeftTurn = false;
-							atLeastOneCard = 0;
-							CardsOfTheSameNumberPerTurn = 0;
+				PressUnoButtonSinglePlayer(even, card, isLeftTurn, atLeastOneCard, isUnobuttonPressed);
 
-							card.checkZeroCardStatement();
-							card.checklimitPlusCard(pluscCardLimitperTurn);
-							card.checkStatementPlusCard(pluscCardLimitperTurn);
-							if (card.getPile().getNumber() == 10 || card.getPile().getNumber() == 11) {
-								card.checkLostTurnCard(isLeftTurn);
-							}
-
-						}
-					}
-				}
-				if (even.type == even.KeyPressed) {
-					if (even.key.code == sf::Keyboard::A) {
-						card.printPileColorAnsNUmber();
-						card.showCardsToEat(isLeftTurn, atLeastOneCard);
-					}
-					if (even.key.code == sf::Keyboard::Q) {
-						cout << " q" << endl;
-						if (isUnobuttonPressed == true) {
-							cout << "true" << endl;
-						}
-						else {
-							cout << "false" << endl;
-						}
-						if (isLeftTurn == true && card.AmountOfCardsInPLayerDeck(true) == 1) {
-							isUnobuttonPressed = true;
-						}
-						if (isUnobuttonPressed == false && card.AmountOfCardsInPLayerDeck(false) == 1) {
-							card.unoButtonPenalty(false);
-
-						}
-					}
-
-				}
-				if (!isEndGame) {
-					if (even.type == even.MouseButtonPressed) {
-						for (int i = 0; i < 24; i++) {
-							{
-								if (card.playerDeckIsPressed(isLeftTurn, i, window, mouse, even)) {
-									card.printPileColorAnsNUmber(i, isLeftTurn);
-									if (card.isNotCardsToEat() || card.isCounterPlusCards(i, isLeftTurn)) {
-
-										card.checkValidCard(i, isLeftTurn, atLeastOneCard, CardsOfTheSameNumberPerTurn, pluscCardLimitperTurn);
-
-									}
-								}
-							}
-						}
-					}
-				}
+				throwCardSinglePlayer(even, window, mouse, card, isLeftTurn, atLeastOneCard, pluscCardLimitperTurn, isEndGame,
+					CardsOfTheSameNumberPerTurn);
 
 
 			}
 
-			if (!isLeftTurn) {
-				if ((isUnobuttonPressed == false && card.AmountOfCardsInPLayerDeck(true) == 1) && !isLeftTurn) {
-					card.unoButtonPenalty(true);
-
-				}
-
-				for (int i = 0; i < 24; i++) {
-
-
-
-					if (card.isNotCardsToEat() || card.isCounterPlusCards(i, isLeftTurn)) {
-
-						card.checkValidCard(i, isLeftTurn, atLeastOneCard, CardsOfTheSameNumberPerTurn, pluscCardLimitperTurn);
-
-						card.checkStatementColorChange(rand() & 3);
-
-						if (isLeftTurn == false && card.AmountOfCardsInPLayerDeck(isLeftTurn) == 1) {
-							isUnobuttonPressed = true;
-						}
-					}
-
-
-				}
-
-				if ((isLeftTurn == false && atLeastOneCard != 0) && card.isNotCardsToEat() == true ||
-					((isLeftTurn == false && atLeastOneCard != 0) && (card.getPile().getNumber() == 12 || card.getPile().getNumber() == 16))) {
-
-					isLeftTurn = true;
-
-					CardsOfTheSameNumberPerTurn = 0;
-					atLeastOneCard = 0;
-
-					card.checkZeroCardStatement();
-					card.checklimitPlusCard(pluscCardLimitperTurn);
-					card.checkStatementPlusCard(pluscCardLimitperTurn);
-					if (card.getPile().getNumber() == 10 || card.getPile().getNumber() == 11) {
-						card.checkLostTurnCard(isLeftTurn);
-					}
-
-				}
-				if (!isLeftTurn) {
-					card.getTypeCard(color, numCard, isLeftTurn);
-
-				}
-
-
-			}
+			playComputerTurn(even, card, isLeftTurn, atLeastOneCard, CardsOfTheSameNumberPerTurn, pluscCardLimitperTurn, isUnobuttonPressed,
+				color, numCard);
 
 
 			if (card.AmountOfCardsInPLayerDeck(true) > 1 && card.AmountOfCardsInPLayerDeck(false) > 1) {
@@ -301,115 +217,17 @@ void multiPlayerWindow(sf::RenderWindow& window, Card& card, sf::Mouse& mouse, b
 
 			while (window.pollEvent(even)) {
 
-				if (even.type == even.Closed) {
+				closeWindowMultiPlayer(even,window,isMultiPlayer);
 
-					window.close();
-					isMultiPlayer = false;
+				startGamee(even,window,card,mouse,color,numCard,startGame);
 
-				}
-				if (even.type == even.MouseButtonPressed && card.isButtonPressed(2, window, mouse, even)) {
-					card.initPileCard(color, numCard);
-					card.init8CardsPerDeck();
-					startGame = true;
-					card.RemoveButton(2);
-				}
+				getCard(even, window, card, mouse, color, numCard,isEndGame,isLeftTurn);
 
-				if (!isEndGame) {
-					if (even.type == even.MouseButtonPressed) {
-						if (card.mainDeckIsPressed(window, mouse, even)) {
-							card.getTypeCard(color, numCard, isLeftTurn);
-						}
-					}
-				}
-				if (even.type == even.KeyPressed) {
-					if (even.key.code == sf::Keyboard::Enter) {
-						if ((isLeftTurn == true && atLeastOneCard != 0) && card.isNotCardsToEat() == true ||
-							((isLeftTurn == true && atLeastOneCard != 0) && (card.getPile().getNumber() == 12 || card.getPile().getNumber() == 16))) {
+				endTheTurnMultiPlayer(even, card, isLeftTurn, atLeastOneCard, CardsOfTheSameNumberPerTurn, pluscCardLimitperTurn);
+				
+				PressUnoButtonMultiPlayer(even,card,isLeftTurn,atLeastOneCard,isUnobuttonPressed);
 
-							isLeftTurn = false;
-							atLeastOneCard = 0;
-							CardsOfTheSameNumberPerTurn = 0;
-
-							card.checkZeroCardStatement();
-							card.checklimitPlusCard(pluscCardLimitperTurn);
-							card.checkStatementPlusCard(pluscCardLimitperTurn);
-							if (card.getPile().getNumber() == 10 || card.getPile().getNumber() == 11) {
-								card.checkLostTurnCard(isLeftTurn);
-							}
-
-						}
-						if ((isLeftTurn == false && atLeastOneCard != 0) && card.isNotCardsToEat() == true ||
-							((isLeftTurn == false && atLeastOneCard != 0) && (card.getPile().getNumber() == 12 || card.getPile().getNumber() == 16))) {
-
-							isLeftTurn = true;
-							CardsOfTheSameNumberPerTurn = 0;
-							atLeastOneCard = 0;
-
-							card.checkZeroCardStatement();
-							card.checklimitPlusCard(pluscCardLimitperTurn);
-							card.checkStatementPlusCard(pluscCardLimitperTurn);
-							if (card.getPile().getNumber() == 10 || card.getPile().getNumber() == 11) {
-								card.checkLostTurnCard(isLeftTurn);
-							}
-
-						}
-					}
-				}
-				if (even.type == even.KeyPressed) {
-					if (even.key.code == sf::Keyboard::A) {
-						card.printPileColorAnsNUmber();
-						card.showCardsToEat(isLeftTurn, atLeastOneCard);
-					}
-					if (even.key.code == sf::Keyboard::Q) {
-						cout << " q" << endl;
-						if (isUnobuttonPressed == true) {
-							cout << "true" << endl;
-						}
-						else {
-							cout << "false" << endl;
-						}
-						if (isLeftTurn == true && card.AmountOfCardsInPLayerDeck(true) == 1) {
-							isUnobuttonPressed = true;
-						}
-						if (isUnobuttonPressed == false && card.AmountOfCardsInPLayerDeck(false) == 1) {
-							card.unoButtonPenalty(false);
-
-						}
-					}
-					if (even.key.code == sf::Keyboard::P) {
-						cout << " p" << endl;
-						if (isUnobuttonPressed == true) {
-							cout << "true" << endl;
-						}
-						else {
-							cout << "false" << endl;
-						}
-
-						if (isLeftTurn == false && card.AmountOfCardsInPLayerDeck(isLeftTurn) == 1) {
-							isUnobuttonPressed = true;
-						}
-						if (isUnobuttonPressed == false && card.AmountOfCardsInPLayerDeck(true) == 1) {
-							card.unoButtonPenalty(true);
-
-						}
-					}
-				}
-				if (!isEndGame) {
-					if (even.type == even.MouseButtonPressed) {
-						for (int i = 0; i < 24; i++) {
-							{
-								if (card.playerDeckIsPressed(isLeftTurn, i, window, mouse, even)) {
-									card.printPileColorAnsNUmber(i, isLeftTurn);
-									if (card.isNotCardsToEat() || card.isCounterPlusCards(i, isLeftTurn)) {
-
-										card.checkValidCard(i, isLeftTurn, atLeastOneCard, CardsOfTheSameNumberPerTurn, pluscCardLimitperTurn);
-
-									}
-								}
-							}
-						}
-					}
-				}
+				throwCardMultiPlayer(even,window,mouse,card,isLeftTurn,atLeastOneCard,pluscCardLimitperTurn,isEndGame,CardsOfTheSameNumberPerTurn);
 
 
 			}
@@ -434,6 +252,274 @@ void multiPlayerWindow(sf::RenderWindow& window, Card& card, sf::Mouse& mouse, b
 
 			window.display();
 		}
+
+	}
+}
+
+void closeWindowSinglePlayer(sf::Event& even, sf::RenderWindow& window, bool& isSinglePlayer)
+{
+	if (even.type == even.Closed) {
+
+		window.close();
+		isSinglePlayer = false;
+
+	}
+}
+
+void closeWindowMultiPlayer(sf::Event& even, sf::RenderWindow& window, bool& isMultiPlayer)
+{
+	if (even.type == even.Closed) {
+
+		window.close();
+		isMultiPlayer = false;
+
+	}
+}
+
+void startGamee(sf::Event& even, sf::RenderWindow& window, Card& card, sf::Mouse& mouse, int& color, int& numCard,bool& startGame)
+{
+	if (even.type == even.MouseButtonPressed && card.isButtonPressed(2, window, mouse, even)) {
+		card.initPileCard(color, numCard);
+		card.init8CardsPerDeck();
+		startGame = true;
+		card.RemoveButton(2);
+	}
+}
+
+void getCard(sf::Event& even, sf::RenderWindow& window, Card& card, sf::Mouse& mouse, int& color, int& numCard, bool& isEndGame, bool& isLeftTurn)
+{
+	if (!isEndGame) {
+		if (even.type == even.MouseButtonPressed) {
+			if (card.mainDeckIsPressed(window, mouse, even)) {
+				card.getTypeCard(color, numCard, isLeftTurn);
+			}
+		}
+	}
+}
+
+void endTheTurnMultiPlayer(sf::Event& even, Card& card, bool& isLeftTurn, int& atLeastOneCard, int& CardsOfTheSameNumberPerTurn, 
+	int& pluscCardLimitperTurn)
+{
+	if (even.type == even.KeyPressed) {
+		if (even.key.code == sf::Keyboard::Enter) {
+			if ((isLeftTurn == true && atLeastOneCard != 0) && card.isNotCardsToEat() == true ||
+				((isLeftTurn == true && atLeastOneCard != 0) && (card.getPile().getNumber() == 12 || card.getPile().getNumber() == 16))) {
+
+				isLeftTurn = false;
+				atLeastOneCard = 0;
+				CardsOfTheSameNumberPerTurn = 0;
+
+				card.checkZeroCardStatement();
+				card.checklimitPlusCard(pluscCardLimitperTurn);
+				card.checkStatementPlusCard(pluscCardLimitperTurn);
+				if (card.getPile().getNumber() == 10 || card.getPile().getNumber() == 11) {
+					card.checkLostTurnCard(isLeftTurn);
+				}
+
+			}
+			if ((isLeftTurn == false && atLeastOneCard != 0) && card.isNotCardsToEat() == true ||
+				((isLeftTurn == false && atLeastOneCard != 0) && (card.getPile().getNumber() == 12 || card.getPile().getNumber() == 16))) {
+
+				isLeftTurn = true;
+				CardsOfTheSameNumberPerTurn = 0;
+				atLeastOneCard = 0;
+
+				card.checkZeroCardStatement();
+				card.checklimitPlusCard(pluscCardLimitperTurn);
+				card.checkStatementPlusCard(pluscCardLimitperTurn);
+				if (card.getPile().getNumber() == 10 || card.getPile().getNumber() == 11) {
+					card.checkLostTurnCard(isLeftTurn);
+				}
+
+			}
+		}
+	}
+}
+
+void endTheTurnSinglePlayer(sf::Event& even, Card& card, bool& isLeftTurn, int& atLeastOneCard, int& CardsOfTheSameNumberPerTurn,
+	int& pluscCardLimitperTurn)
+{
+	if (even.type == even.KeyPressed) {
+		if (even.key.code == sf::Keyboard::Enter) {
+			if ((isLeftTurn == true && atLeastOneCard != 0) && card.isNotCardsToEat() == true ||
+				((isLeftTurn == true && atLeastOneCard != 0) && (card.getPile().getNumber() == 12 || card.getPile().getNumber() == 16))) {
+
+				isLeftTurn = false;
+				atLeastOneCard = 0;
+				CardsOfTheSameNumberPerTurn = 0;
+
+				card.checkZeroCardStatement();
+				card.checklimitPlusCard(pluscCardLimitperTurn);
+				card.checkStatementPlusCard(pluscCardLimitperTurn);
+				if (card.getPile().getNumber() == 10 || card.getPile().getNumber() == 11) {
+					card.checkLostTurnCard(isLeftTurn);
+				}
+
+			}
+		}
+	}
+}
+
+void PressUnoButtonMultiPlayer(sf::Event& even, Card& card, bool& isLeftTurn, int& atLeastOneCard, bool& isUnobuttonPressed)
+{
+	if (even.type == even.KeyPressed) {
+		if (even.key.code == sf::Keyboard::A) {
+			card.printPileColorAnsNUmber();
+			card.showCardsToEat(isLeftTurn, atLeastOneCard);
+		}
+		if (even.key.code == sf::Keyboard::Q) {
+			cout << " q" << endl;
+			if (isUnobuttonPressed == true) {
+				cout << "true" << endl;
+			}
+			else {
+				cout << "false" << endl;
+			}
+			if (isLeftTurn == true && card.AmountOfCardsInPLayerDeck(true) == 1) {
+				isUnobuttonPressed = true;
+			}
+			if (isUnobuttonPressed == false && card.AmountOfCardsInPLayerDeck(false) == 1) {
+				card.unoButtonPenalty(false);
+
+			}
+		}
+		if (even.key.code == sf::Keyboard::P) {
+			cout << " p" << endl;
+			if (isUnobuttonPressed == true) {
+				cout << "true" << endl;
+			}
+			else {
+				cout << "false" << endl;
+			}
+
+			if (isLeftTurn == false && card.AmountOfCardsInPLayerDeck(isLeftTurn) == 1) {
+				isUnobuttonPressed = true;
+			}
+			if (isUnobuttonPressed == false && card.AmountOfCardsInPLayerDeck(true) == 1) {
+				card.unoButtonPenalty(true);
+
+			}
+		}
+	}
+}
+
+void PressUnoButtonSinglePlayer(sf::Event& even, Card& card, bool& isLeftTurn, int& atLeastOneCard, bool& isUnobuttonPressed)
+{
+	if (even.type == even.KeyPressed) {
+		if (even.key.code == sf::Keyboard::A) {
+			card.printPileColorAnsNUmber();
+			card.showCardsToEat(isLeftTurn, atLeastOneCard);
+		}
+		if (even.key.code == sf::Keyboard::Q) {
+			cout << " q" << endl;
+			if (isUnobuttonPressed == true) {
+				cout << "true" << endl;
+			}
+			else {
+				cout << "false" << endl;
+			}
+			if (isLeftTurn == true && card.AmountOfCardsInPLayerDeck(true) == 1) {
+				isUnobuttonPressed = true;
+			}
+			if (isUnobuttonPressed == false && card.AmountOfCardsInPLayerDeck(false) == 1) {
+				card.unoButtonPenalty(false);
+
+			}
+		}
+
+	}
+}
+
+void throwCardMultiPlayer(sf::Event& even, sf::RenderWindow& window, sf::Mouse& mouse, Card& card, bool& isLeftTurn, int& atLeastOneCard,
+	int& pluscCardLimitperTurn, bool& isEndGame, int& CardsOfTheSameNumberPerTurn)
+{
+	if (!isEndGame) {
+		if (even.type == even.MouseButtonPressed) {
+			for (int i = 0; i < 24; i++) {
+				{
+					if (card.playerDeckIsPressed(isLeftTurn, i, window, mouse, even)) {
+						card.printPileColorAnsNUmber(i, isLeftTurn);
+						if (card.isNotCardsToEat() || card.isCounterPlusCards(i, isLeftTurn)) {
+
+							card.checkValidCard(i, isLeftTurn, atLeastOneCard, CardsOfTheSameNumberPerTurn, pluscCardLimitperTurn);
+
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+void throwCardSinglePlayer(sf::Event& even, sf::RenderWindow& window, sf::Mouse& mouse, Card& card, bool& isLeftTurn, int& atLeastOneCard,
+	int& pluscCardLimitperTurn, bool& isEndGame, int& CardsOfTheSameNumberPerTurn)
+{
+	if (!isEndGame) {
+		if (even.type == even.MouseButtonPressed) {
+			for (int i = 0; i < 24; i++) {
+				{
+					if (card.playerDeckIsPressed(isLeftTurn, i, window, mouse, even)) {
+						card.printPileColorAnsNUmber(i, isLeftTurn);
+						if (card.isNotCardsToEat() || card.isCounterPlusCards(i, isLeftTurn)) {
+
+							card.checkValidCard(i, isLeftTurn, atLeastOneCard, CardsOfTheSameNumberPerTurn, pluscCardLimitperTurn);
+
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+void playComputerTurn(sf::Event& even, Card& card, bool& isLeftTurn, int& atLeastOneCard, int& CardsOfTheSameNumberPerTurn,
+	int& pluscCardLimitperTurn, bool& isUnobuttonPressed, int& color ,int& numCard)
+{
+	if (!isLeftTurn) {
+		if ((isUnobuttonPressed == false && card.AmountOfCardsInPLayerDeck(true) == 1) && !isLeftTurn) {
+			card.unoButtonPenalty(true);
+
+		}
+
+		for (int i = 0; i < 24; i++) {
+
+
+
+			if (card.isNotCardsToEat() || card.isCounterPlusCards(i, isLeftTurn)) {
+
+				card.checkValidCard(i, isLeftTurn, atLeastOneCard, CardsOfTheSameNumberPerTurn, pluscCardLimitperTurn);
+
+				card.checkStatementColorChange(rand() & 3);
+
+				if (isLeftTurn == false && card.AmountOfCardsInPLayerDeck(isLeftTurn) == 1) {
+					isUnobuttonPressed = true;
+				}
+			}
+
+
+		}
+
+		if ((isLeftTurn == false && atLeastOneCard != 0) && card.isNotCardsToEat() == true ||
+			((isLeftTurn == false && atLeastOneCard != 0) && (card.getPile().getNumber() == 12 || card.getPile().getNumber() == 16))) {
+
+			isLeftTurn = true;
+
+			CardsOfTheSameNumberPerTurn = 0;
+			atLeastOneCard = 0;
+
+			card.checkZeroCardStatement();
+			card.checklimitPlusCard(pluscCardLimitperTurn);
+			card.checkStatementPlusCard(pluscCardLimitperTurn);
+			if (card.getPile().getNumber() == 10 || card.getPile().getNumber() == 11) {
+				card.checkLostTurnCard(isLeftTurn);
+			}
+
+		}
+		if (!isLeftTurn) {
+			card.getTypeCard(color, numCard, isLeftTurn);
+
+		}
+
 
 	}
 }
